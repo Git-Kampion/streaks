4d3862e101c0b57869123c5cbbbdd9e872a40f08
Git-Kampion <122305375+Git-Kampion@users.noreply.github.com>
2026-05-11 14:38:28 +0200

    Add files via upload
    20260511

--- importResV3git.py
+++ importResV3git.py
@@ -81,37 +81,40 @@ cookiRan = False
 
 for lnk in content:
     linnk = lnk.split(" ")[0].strip()
-    if linnk and linnk != "#":
+    if linnk != "#" and linnk != " ":
         try:
             browser.get(linnk)
-        except WebDriverException:
-            continue
-
+        except:
+            done = 0
+            break;
+        def is_ready(browser):
+            return browser.execute_script("return document.readyState === 'complete'")
         WebDriverWait(browser, 30).until(is_ready)
-        time.sleep(3)
-        remove_ads(browser)
+
+        browser.maximize_window()
+        time.sleep(10)
 
         # Handle cookies banner once
-        if not cookiRan:
+        if cookiRan is False:
             try:
-                findCookie = WebDriverWait(browser, 10).until(
-                    EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
-                )
+                findCookie = browser.find_element(By.ID, "onetrust-accept-btn-handler")
+
                 findCookie.click()
                 cookiRan = True
                 time.sleep(2)
             except (NoSuchElementException, TimeoutException):
                 pass
 
-        # Keep clicking "Show more matches" until none remain
-        while True:
-            clicked = safe_click(browser, By.LINK_TEXT, "Show more matches")
-            if not clicked:
-                break
-            time.sleep(5)
-
-        time.sleep(2)
-        remove_ads(browser)
+        # Keep clicking "Show more matches"
+        _flag = True
+        while _flag:
+            try:
+                browser.find_element(By.LINK_TEXT, "Show more matches").click()
+                time.sleep(10)
+                _flag = True
+            except NoSuchElementException:
+                _flag = False
+        time.sleep(5)
 
         matchesPopUps = browser.find_elements(By.CLASS_NAME, "eventRowLink")
         iteloop = dataLookUp(lnk.split()[2], len(matchesPopUps))
@@ -124,75 +127,90 @@ for lnk in content:
                     browser.execute_script(f"window.open('{href}', '_blank');")
                     time.sleep(5)
                 browser.switch_to.window(browser.window_handles[-1])
+                success = False
+                time.sleep(10)
+                
+                bjj = browser.find_element(By.CLASS_NAME, "detail__breadcrumbs")
+                list_items = bjj.find_elements(By.TAG_NAME, "li")
+                bjj = list_items[2].text
 
+                ull = browser.find_element(By.CLASS_NAME, "duelParticipant").text.split("\n")
+                dt = ull[0].split(" ")[0]
+                Hteam = ull[1].replace(" ", "")
+                Ateam = ull[6].replace(" ", "")
                 try:
-                    bjj = browser.find_element(By.CLASS_NAME, "detail__breadcrumbs")
-                    list_items = bjj.find_elements(By.TAG_NAME, "li")
-                    bjj = list_items[2].text
-
-                    ull = browser.find_element(By.CLASS_NAME, "duelParticipant").text.split("\n")
-                    dt = ull[0].split(" ")[0]
-                    Hteam = ull[1].replace(" ", "")
-                    Ateam = ull[6].replace(" ", "")
-                    try:
                         wjj = bjj.index("ROUND")
                         round = bjj[wjj:].split(" ")[1]
-                    except:
+                        round2 = int(round)
+                except:
                         round = "1"
 
-                    if "PLAY" not in bjj and "GROUP" not in bjj and "FINAL" not in bjj:
-                        success = False
-                        while not success:
+                if "PLAY" not in bjj and "GROUP" not in bjj and "FINAL" not in bjj:
+                        while success != True:
                             try:
                                 kjj = browser.find_element(By.CLASS_NAME, "smv__verticalSections").text.split()
                                 success = True
                             except:
                                 time.sleep(1)
 
-                        home1stHScore = ""
-                        away1stHScore = ""
-                        home2ndHScore = ull[2]
-                        away2ndHScore = ull[4]
+                            home1stHScore = ""
+                            away1stHScore = ""
+                            home2ndHScore = ull[2]
+                            away2ndHScore = ull[4]
 
-                        ndFound = False
-                        ndfoundSec = False
-                        for id in kjj:
-                            if id == "1ST":
-                                ndFound = True
-                            elif ndFound and id != "HALF":
-                                home1stHScore = id
-                                away1stHScore = kjj[kjj.index("1ST") + 4]
-                                ndFound = False
-                            if id == "2ND":
-                                ndfoundSec = True
-                            elif ndfoundSec and id != "HALF":
-                                ndfoundSec = False
+                            ndFound = False
+                            ndfoundSec = False
+                            for id in kjj:
+                                if id == "1ST":
+                                    ndFound = True
+                                else:
+                                    if ndFound == True and id != "HALF":
+                                        home1stHScore = id
+                                        away1stHScore = kjj[kjj.index("1ST") + 4]
+                                        if home1stHScore == '' and away1stHScore == '':
+                                            home1stHScore = 0
+                                            away1stHScore = 0
+                                        ndFound = False
+                                if id == "2ND":
+                                    ndfoundSec = True
+                                else:
+                                    if ndfoundSec == True and id != "HALF":
+                                        ndfoundSec = False
 
-                        dttyl = f"{round} {dt} {Hteam} {Ateam} {home2ndHScore} {away2ndHScore} {home1stHScore} {away1stHScore}"
-                        conn = sqlite3.connect(DB2)
-                        cursor = conn.cursor()
+                        dttyl = round + " " + dt + " " + Hteam + " " + Ateam + " " + home2ndHScore + " " +  away2ndHScore + " " + home1stHScore  + " " + away1stHScore
+                        if len(dttyl.split(" ")) > 0:
+                            conn = sqlite3.connect(DB2)
+                            cursor = conn.cursor()
 
-                        polstriped = dttyl.split(" ")
-                        insert_stmt = f"""
-                            INSERT INTO {lnk.split(" ")[2]}
-                            (Round, Tframe, home, away, hgoal, agoal, hhgoal, ahgoal)
-                            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                        """
+                            polstriped = dttyl.split(" ")
+                            
+                            insert_stmt = f"""
+                                INSERT INTO {lnk.split(" ")[2]}
+                                (Round, Tframe, home, away, hgoal, agoal, hhgoal, ahgoal)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                            """
 
-                        try:
-                            cursor.execute(insert_stmt, polstriped[:8])
-                        except Exception:
-                            fallback = (polstriped[0], polstriped[1], polstriped[3], polstriped[4],
-                                        polstriped[5], polstriped[6], '0', '0')
-                            cursor.execute(insert_stmt, fallback)
+                            if "Penalty" in polstriped[2]:
+                                data = (polstriped[0],polstriped[1],polstriped[3],polstriped[4],
+                                        polstriped[5],polstriped[6],polstriped[7],polstriped[8])
+                            else:
+                                data = (polstriped[0],polstriped[1],polstriped[2],polstriped[3],
+                                        polstriped[4],polstriped[5],polstriped[6],polstriped[7])
 
-                        conn.commit()
-                        cursor.close()
-                        conn.close()
+                            try:
+                                cursor.execute(insert_stmt, data)
+                            except Exception as e:
+                                # fallback if issue
+                                data = (polstriped[0],polstriped[1],polstriped[3],polstriped[4],
+                                        polstriped[5],polstriped[6],'0','0')
+                                cursor.execute(insert_stmt, data)
 
-                finally:
-                    browser.close()
-                    browser.switch_to.window(browser.window_handles[0])
+                            conn.commit()
+                            cursor.close()
+                            conn.close()
+
+                firstWindow = browser.window_handles[0]
+                browser.close()
+                browser.switch_to.window(firstWindow)   
             countUps -= 1
-
 browser.quit()
